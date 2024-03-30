a731e03bc32b3cb424ab97c18d169e55e316c408
northernerwolf <guwanchaldurdyyew01@gmail.com>
2024-03-30 09:15:20 +0500

    Initial Commit

---
+++ wordl/wordlApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordlApp.swift
+//  wordl
+//
+//  Created by Guwanch Haldurdyyev on 30.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct wordlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
